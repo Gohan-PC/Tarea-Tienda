2cc1df927da314ebb8a8dab9a68f6430910ba823
Gohan-PC <56096629+Gohan-PC@users.noreply.github.com>
2019-10-15 16:30:42 -0500

    Primer commit

---
+++ Producto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zineb
+ */
+public class Producto {
+	String nombre;
+	String codigo;
+	Double  precio;
+	public Producto(String nombre, String codigo, Double precio) {
+		super();
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.precio = precio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	
+}
